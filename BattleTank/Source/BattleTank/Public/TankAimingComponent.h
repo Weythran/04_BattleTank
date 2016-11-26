a0bf88b19a8c778d3898a880c793fe8065b7347a
Stan Tenkovskiy <tenkovskiy@yahoo.com>
2016-11-25 19:25:54 -0500

    159. Using Enum(erations) in UE4

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -5,6 +5,14 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8 {
+	Reloading,
+	Aiming,
+	Locked
+};
+
 // Forward declaration
 class UTankBarrel;
 class UTankTurret;
@@ -24,6 +32,10 @@ public:
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 
 	UTankBarrel* Barrel = nullptr;
@@ -31,4 +43,5 @@ private:
 
 	UTankTurret* Turret = nullptr;
 	void MoveTurretTowards(FVector AimDirection);
+
 };
